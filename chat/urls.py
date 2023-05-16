1f3bf35d0097ff1d59c2a3025ba14955b99351d7
wnesbv <wnesbv@protonmail.com>
2023-05-16 14:47:22 +0300

    make it better

--- chat/urls.py
+++ chat/urls.py
@@ -18,6 +18,7 @@ JWT_KEY = "!0_77!%_#)p3gk-m_np8sukvi1^9_^38s^l-g505fsqg-1j&2&"
 sio = socketio.Server(
     logger=True,
     async_mode="gevent",
+    cookie=("userio")
 )
 
 
@@ -88,30 +89,6 @@ def save_img(upload, user):
 # ..
 
 
-@chat.route("/")
-@visited()
-def chat_all_get():
-    token = request.get_cookie("visited")
-    user_list = who_is_who()[2]
-    # ..
-    cur = con.cursor()
-    cur.execute("SELECT * FROM chat_table")
-    res = cur.fetchall()
-    cur.execute("SELECT * FROM journal ORDER BY - id")
-    i_journal = cur.fetchall()
-    cur.close()
-    return template(
-        "chat/chat.html",
-        res=res,
-        token=token,
-        user_list=user_list,
-        i_journal=i_journal,
-    )
-
-
-# ..
-
-
 @sio.event
 def my_event(sid, message):
     sio.emit("my_response", {"data": message["data"]}, room=sid)
@@ -133,21 +110,24 @@ def my_broadcast_event(sid, message):
 
 clients = 0
 
+
+
 @sio.event
 def connect(sid, headers):
+
     global clients
     clients += 1
+
 
     sio.emit(
         "my_response", {"data": "Connected..!", "count": clients}, room=sid
     )
     sio.emit(
-        "email", {"user": user_visited(headers)}
-    )
+            "entrance", {"email": user_visited(headers)}
+        )
 
 
     print(f" Client connect..! sid.. {sid} clients.. {clients}")
-    print(f"user headers.. {user_visited(headers)}")
 
     save_journal(headers)
 
@@ -164,15 +144,22 @@ def disconnect(sid):
     global clients
     clients -= 1
 
-    print(f" Client disconnected..! sid.. {sid} clients.. {clients}")
-
     user_cookie = sio.get_environ(sid, "/")["HTTP_COOKIE"]
 
-    original = user_cookie
-    removed = original.replace("visited=", "")
+    removed = user_cookie.replace(";", "")
+    i_token = dict(i.split("=") for i in removed.split())
+    token = i_token.get("visited")
 
-    item_user = jwt.decode(removed, JWT_KEY, JWT_ALGORITHM)
-    for_user = item_user["mail"]
+    coockie_visited = jwt.decode(token, JWT_KEY, JWT_ALGORITHM)
+    for_user = coockie_visited["mail"]
+
+    # ..
+    sio.emit(
+            "exit", {"user": for_user}
+        )
+    # ..
+
+    print(f" Client disconnected..! sid.. {sid} clients.. {clients}")
 
     end = datetime.now()
     f_end = end.strftime(F_TIME)
@@ -229,3 +216,27 @@ def my_room_event(sid, message):
 def disconnect_request(sid):
     sio.disconnect(sid)
     print(f"disconnect_request.. {sid}")
+
+
+# ..
+
+
+@chat.route("/")
+@visited()
+def chat_all_get():
+    token = request.get_cookie("visited")
+    user_list = who_is_who()[2]
+    # ..
+    cur = con.cursor()
+    cur.execute("SELECT * FROM chat_table")
+    res = cur.fetchall()
+    cur.execute("SELECT * FROM journal ORDER BY - id")
+    i_journal = cur.fetchall()
+    cur.close()
+    return template(
+        "chat/chat.html",
+        res=res,
+        token=token,
+        user_list=user_list,
+        i_journal=i_journal,
+    )
